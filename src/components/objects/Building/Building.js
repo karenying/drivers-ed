2e9f1247442493651d8af253832c1ab9eed0f5ca
Dora Zhao <dorothyz@princeton.edu>
2020-05-02 21:08:55 -0400

    cap

--- src/components/objects/Building/Building.js
+++ src/components/objects/Building/Building.js
@@ -15,7 +15,22 @@ var Colors = {
   white: 0xffebc4,
   cement: 0xdbca9a,
   wood: 0x4d2800,
+  darkGreen:0x00472e,
+  brown:0x4a2a0a,
 };
+
+function createCylinder(x, y, z, s, color, dx, dy, dz) {
+  let geo = new CylinderGeometry(x, y, z, s);
+  let mat = new MeshToonMaterial({
+    color: color,
+    flatShading: true,
+  });
+  let mesh = new Mesh(geo, mat);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  mesh.position.set(dx, dy, dz);
+  return mesh;
+}
 
 function makeMesh(geo, mat, dx, dy, dz) {
   let mesh = new Mesh(geo, mat);
@@ -25,6 +40,22 @@ function makeMesh(geo, mat, dx, dy, dz) {
   return mesh;
 }
 
+function makeWindow(x, y, geo, mat, object) {
+  for (let i = 0; i < 4; i++) {
+    let xCoord;
+    let yCoord = y;
+    if (i % 2 == 1) {
+      xCoord= -x;
+    } else {
+      xCoord = x;
+    }
+    if (i >= 2) {
+      yCoord = -y;
+    }
+    let pane = makeMesh(geo, mat, xCoord, yCoord, -0.01);
+    object.add(pane);
+  }
+}
 class Building extends Group {
 
   constructor(parent) {
@@ -44,6 +75,7 @@ class Building extends Group {
         flatShading: true,
       });
       let building = makeMesh(buildingGeo, buildingMat, 0, 0, 0);
+
       // doors
       let doorGeo = new PlaneGeometry(0.4, 0.7, 0.2);
       let doorMat = new MeshToonMaterial({
@@ -62,14 +94,14 @@ class Building extends Group {
       let door6 = makeMesh(doorGeo, doorMat, -2, 0, 0);
 
       // make windows
-      let windows = makeMesh(windowGeo, doorMat, -2.4, 0.4, -1.6);
+      let windows = makeMesh(windowGeo, doorMat, -2.4, 0.4, -1.51);
       let xDist = 0.6;
       for (let i = 0; i < 8; i++) {
         if (xDist == 0) {
           xDist += 0.6;
           continue;
         }
-        let newWindow = makeMesh(windowGeo, doorMat, xDist, 0, 0);
+        let newWindow = makeMesh(windowGeo, doorMat, xDist, 0, 0.0);
         windows.add(newWindow);
         xDist += 0.6;
       }
@@ -164,7 +196,7 @@ class Building extends Group {
       let building = makeMesh(buildingGeo, buildingMat, 0, 0, 0);
 
       // make door
-      let doorFrameGeo = new BoxGeometry(1.5, 1.5, 0.1);
+      let doorFrameGeo = new BoxGeometry(1.25, 1.5, 0.1);
       let doorFrame = makeMesh(doorFrameGeo, cementMat, 0, -0.75, -1.51);
       let doorGeo = new PlaneGeometry(0.75, 0.7, 0.01);
       let doorTopGeo = new CircleGeometry(0.375, 32);
@@ -176,22 +208,31 @@ class Building extends Group {
       // windows
       let topWindowFrameGeo = new PlaneGeometry(0.6, 0.8, 0.01);
       let topWindow = makeMesh(topWindowFrameGeo, cementMat, 0, 0.6, -1.51);
+
+      // front door window
       let windowPaneGeo = new PlaneGeometry(0.25, 0.35, 0.01);
-      for (let i = 0; i < 4; i++) {
-        let x = 0.2;
-        let y = 0.2;
-        if (i % 2 == 1) {
-          x = -0.15;
-        } else {
-          x = 0.15;
-        }
-        if (i >= 2) {
-          y = -0.2;
-        }
-        let pane = makeMesh(windowPaneGeo, windowMat, x, y, -0.01);
-        topWindow.add(pane);
+      makeWindow(0.15, 0.2, windowPaneGeo, windowMat, topWindow);
+
+      let shift = 1.2;
+      let rightWindow = makeMesh(topWindowFrameGeo, cementMat, 1, 0, 0);
+      makeWindow(0.15, 0.2, windowPaneGeo, windowMat, rightWindow);
+      topWindow.add(rightWindow);
+      for (let i = 0; i < 3; i++) {
+        let newWindow = makeMesh(topWindowFrameGeo, cementMat, -shift, 0, 0);
+        makeWindow(0.15, 0.2, windowPaneGeo, windowMat, newWindow);
+        let newWindow2 = makeMesh(topWindowFrameGeo, cementMat, -shift, -1.25, 0);
+        makeWindow(0.15, 0.2, windowPaneGeo, windowMat, newWindow2);
+        topWindow.add(newWindow, newWindow2);
+        shift += 0.9;
       }
-      building.add(doorFrame, topWindow);
+      // side windows
+      let sideWindow1 = makeMesh(topWindowFrameGeo, cementMat, 2.5, 1, -3.01);
+      makeWindow(0.15, 0.2, windowPaneGeo, windowMat, sideWindow1);
+      let largeWindowGeo = new PlaneGeometry(1.2, 1.2, 0.01);
+      let sideWindow2 = makeMesh(largeWindowGeo, cementMat, 2.5, -0.5, -3.01);
+      let squareWindowGeo = new PlaneGeometry(0.5, 0.5, 0.01);
+      makeWindow(0.27, 0.27, squareWindowGeo, windowMat, sideWindow2);
+      building.add(doorFrame, topWindow, sideWindow1, sideWindow2);
 
       // roof
       let roof1Geo = new BoxGeometry(7, 1, 3);
@@ -232,7 +273,16 @@ class Building extends Group {
       annexRoof.add(annexRoof2);
       annex.add(annexRoof);
       building.add(annex);
-      this.add(building);
+
+      // tree
+      let tree = createCylinder(0.15, 0.15, 1.5, 18, Colors.brown, 0.75, -1.2, -1.7);
+      let branch = createCylinder(0.02, 0.09, 0.6, 18, Colors.brown, 0, 0.2, 0.3);
+      let leaves = createCylinder(1, 0.6, 1.2, 8, Colors.darkGreen, 0, 1.25, 0);
+      branch.geometry.rotateX(Math.PI / 4);
+      tree.add(branch, leaves);
+      tree.rotateY(Math.PI / 4);
+      tree.scale.set(0.5, 0.6, 0.5);
+      this.add(building, tree);
     }
   }
 }
