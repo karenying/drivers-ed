579f8d12c05d445b6bda66cf1aeecebf27dab57d
Dora Zhao <dorothyz@princeton.edu>
2020-05-10 18:26:05 -0400

    grassy

--- src/components/scenes/Washington.js
+++ src/components/scenes/Washington.js
@@ -86,10 +86,7 @@ class Washington extends Scene {
         // lampPositions
         const lampPositions = [
             0,
-            -40,
-            -80,
-            -120,
-            -160,
+            -100,
             -200,
         ];
 
@@ -137,11 +134,11 @@ class Washington extends Scene {
         this.collidableMeshList.push(dan);
 
         // add lamppost
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 3; i++) {
           const lamppostLeft = new Lamppost(this);
           const lamppostRight = new Lamppost(this);
           lamppostLeft.position.set(-5.6, 1.5, lampPositions[i]);
-          lamppostRight.position.set(5.6, 1.5, lampPositions[i] + 20);
+          lamppostRight.position.set(5.6, 1.5, lampPositions[i] + 50);
           this.add(lamppostLeft, lamppostRight);
         }
 
